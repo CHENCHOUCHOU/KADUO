5d8f6a8788f74f116ed5315ebef8526df64d769a
CHENCHOUCHOU <2252350294@qq.com>
2018-04-11 18:29:05 +0800

    fistpage done

---
+++ Assets/Animal Friends/Scripts/MainSceneScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainSceneScript : MonoBehaviour 
+{
+
+      
+        void Start () {
+	
+
+	
+}
+
+ 
+        void Update () {
+		
+
+
+	}
+
+        void onClick () {
+		
+
+
+           SceneManager.LoadScene("MainScene");       
+	}
+
+
+
+}
